8a5af11af0fec10382f794f5cb3beae958864c5b
celebrater <tkgm45nbchk@gmail.com>
2022-03-07 08:56:56 +0900

    test succeeded

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// const getJson = () => {
+//     // const href = location.href; // https://enin-world.sakura.ne.jp/index.html
+//     // const pathname = location.pathname; // /index.html
+//     // const jsonPath = href.replace(pathname, "") + "/test.json";
+//     fetch("./test.json")
+//         .then(response => {
+//             return response.json();
+//         });
+// }
+//
+// // getJson();
+// console.log(getJson());
+
+
+const output = (result) => {
+    console.log("output(): ");
+    console.log(result); // succeeded
+}
+
+// fetch 自体は Promise を返す
+const data = fetch("./test.json")
+    .then((response) => {
+        return response.json()　//ここでBodyからJSONを返す
+    })
+    .then((result) => {
+        output(result);  //取得したJSONデータを関数に渡す
+    })
+    .catch((e) => {
+        console.log(e)  //エラーをキャッチし表示
+    })
+
+console.log(data);　//コンソールに"Promise"と表示される
+
+// Chrome ブラウザをデベロッパーモードで起動していないと CORS エラーとなる
